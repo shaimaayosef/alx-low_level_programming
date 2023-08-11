a6f895fe6797fd168a2cf404843092fd1e180796
shaimaayosef <shimaa.sayed8888@gmail.com>
2023-08-11 16:17:10 -0700

    edit tasks 0,1,9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,14 @@
  *
  * Return: Always 0(Success).
  */
-int main(void)
-{
-	int a;
+int main()
+    for (int i = 0; i < 10; i++) 
+        putchar(i + '0');r
+        if (i < 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	for (a = 0; a <= 9; a++)
-	{
-		putchar((a % 10) + '0');
-		if (a == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
+    return 0;
 }
