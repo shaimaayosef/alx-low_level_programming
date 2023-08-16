af857d5254fa17afc809367116634940df760dac
shaimaayosef <shimaa.sayed8888@gmail.com>
2023-08-15 19:23:16 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * adds two integers and returns the result
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
 
 int add(int a, int b)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,9 @@
 #include "main.h"
 
 /**
- * prints all natural numbers from n to 98.
+ *  print_to_98 - prints all natural numbers from n to 98,
+ *  followed by a new line
+ *  @n: print from this number
  */
 
 void print_to_98(int n)
