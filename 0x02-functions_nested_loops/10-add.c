7d706ca550a643f83d941e6d492f61f51a937743
shaimaayosef <shimaa.sayed8888@gmail.com>
2023-08-15 19:08:32 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * adds two integers and returns the result
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
